1717ac395462ddcf8bd5d731e7da1e1358edac92
Will Brown <williambrown@williams-air.lan>
2019-07-17 23:57:54 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+var shoutouts = [
+  "You are really nice and your websites are amazing!",
+  "You come up with so many great ideas (like making an instagram photo gallery) and always execute them to the best of your ability",
+  "you have really good ideas and are really nice about helping others who need help",
+  "Your creativity. You create such beautiful projects!",
+  "You're so creative! I love the websites you have made.",
+  "how you kept helping me when I asked questions",
+  "I love spending time with you and I am very thankful of our friendship! You are very easy to talk to as well.",
+  "how aesthetically beautiful and well-designed your creations are! I think that's so special that everything you make looks so cool. (Especially your Netflix TV-show generator site!)",
+  "that you have a really good eye for design.",
+  "Your ability to make really aesthetic creative choices and your kindness and willingness to always help out.",
+  "How fun it is to work with you on projects!",
+  "how inspiring your ideas and projects are. You are so nice and helpful and such an inspiration. Thanks for making class so great:)",
+  "Your focus and determination."
+]
+
+for (x of shoutouts) {
+  $("#shoutout-container").append("<p>" + x + "</p> <br>");
+}
